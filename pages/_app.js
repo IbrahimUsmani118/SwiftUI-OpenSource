8adf3767459e487083d352ed81bc0f1e910aba6c
Ibrahim Usmani <78166163+IbrahimUsmani118@users.noreply.github.com>
2024-03-27 19:23:24 +0000

    3 Buttons on the side and top

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 import '../global.css'
+import { useRouter } from 'next/router';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleNavigation = (route) => {
+    router.push(route);
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><button onClick={() => handleNavigation('/about')}>About</button></li>
+          <li><button onClick={() => handleNavigation('/contact')}>Contact</button></li>
+          <li><button onClick={() => handleNavigation('/')}>Home</button></li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
+export default MyApp;
